134538951dead17e0cbc861d18bbcbfd3e4959a1
Mikhail Korotin <korotkiy-mixa85@ya.com>
2013-09-12 02:56:44 +0400

    changed cp command in the script

--- changer.sh
+++ changer.sh
@@ -7,7 +7,7 @@ t="SHRI"
 for i in `ack-grep --recurse --files-with-match --type=html $f ./www`
 do
     reg="s/$f/$t/g"
-    cp $i .${i}.bak
+    cp --force $i ./${i}.bak
     sed=-e $reg $i > $i
     echo $i
 done
