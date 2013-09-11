ebc561f28defd65a6fc04f305ba24d2eb60a364e
Mikhail Korotin <korotkiy-mixa85@ya.com>
2013-09-12 02:25:13 +0400

    First commit

---
+++ changer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+f="BEM"
+t="SHRI"
+
+
+for i in `ack-grep --recurse --files-with-match --type=html $f ./www`
+do
+    reg="s/$f/$t/g"
+    cp $i .${i}.bak
+    sed=-e $reg $i > $i
+    echo $i
+done
